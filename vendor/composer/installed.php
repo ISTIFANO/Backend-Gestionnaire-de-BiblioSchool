6a85bec077b38d75928da167444f812f4e0538b1
istifano <elamiriamir7@gmail.com>
2024-12-30 09:56:40 +0100

    ajouter les fichiers de composer et la configuration du strecture du project

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '1f3a3289723472ad9b3816918db006945c0edf57',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '1f3a3289723472ad9b3816918db006945c0edf57',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'phpdevcommunity/php-dotenv' => array(
+            'pretty_version' => '3.0.1',
+            'version' => '3.0.1.0',
+            'reference' => '896b6b1a1184dddd76e536fed674560f4381b400',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phpdevcommunity/php-dotenv',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
