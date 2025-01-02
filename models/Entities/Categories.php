9bf760e19ceef18bf3900466df94316ec8afa240
istifano <elamiriamir7@gmail.com>
2025-01-02 22:48:09 +0100

    j ai crier le crud de class Categories

--- models/Entities/Categories.php
+++ models/Entities/Categories.php
@@ -4,26 +4,38 @@ class Categories {
     private $id;
     private $name;
     private $created_at;
+    public $connexion;
+
     
     public function __construct($name) {
+        $this->connexion = (new DatabaseConfiguration())::connect();
+
         $this->name = $name;
         $this->created_at = date('Y-m-d H:i:s');
     }
     
-    public function createCategories() {
-       
+    public function create($name) {
+        $query = "INSERT INTO Categories (Name) VALUES ('$name')";
+        return $this->connexion->query($query);
     }
     
-    public function deleteCategories() {
-        
+    public function read($id = null) {
+        if ($id) {
+            $query = "SELECT * FROM Categories WHERE id = $id";
+        } else {
+            $query = "SELECT * FROM Categories";
+        }
+        return $this->connexion->query($query);
     }
     
-    public function listerCategories() {
-    
+    public function update($id, $name) {
+        $query = "UPDATE Categories SET Name = '$name' WHERE id = $id";
+        return $this->connexion->query($query);
     }
     
-    public function updateCategories() {
-        
+    public function delete($id) {
+        $query = "DELETE FROM Categories WHERE id = $id";
+        return $this->connexion->query($query);
     }
 }
 ?>
