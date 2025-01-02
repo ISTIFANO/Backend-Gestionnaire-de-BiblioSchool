4d58c674c99fb1f14b2e90986a6b9b0f885f82fc
istifano <elamiriamir7@gmail.com>
2025-01-02 22:04:51 +0100

    fixer les problemes de phpdotenv et j ai fait la connexion

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 # Backend-Gestionnaire-de-BiblioSchool
-<h1 align="center">
   <a href="https://www.linkedin.com/in/aamir-el-amiri-5672ba262/">
     <img src="./public/images/uml.png">
   </a>
-</h1>
+
--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -33,6 +33,18 @@ class ComposerAutoloaderInitbefa9c52fbb5de5a3f64f38230fd9abd
 
         $loader->register(true);
 
+        $filesToLoad = \Composer\Autoload\ComposerStaticInitbefa9c52fbb5de5a3f64f38230fd9abd::$files;
+        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
+            if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+                $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+                require $file;
+            }
+        }, null, null);
+        foreach ($filesToLoad as $fileIdentifier => $file) {
+            $requireFile($fileIdentifier, $file);
+        }
+
         return $loader;
     }
 }
